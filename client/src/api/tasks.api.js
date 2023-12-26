f2066977c95bb9997cc78ea79de3c315019db1f6
Jmond544 <jmondalgotapia@gmail.com>
2023-12-26 13:10:20 -0500

    Setting to deploy on fronted

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,27 +1,28 @@
 import axios from "axios";
+import {API_URL} from "./config.js"
 
 export const createTaskRequest = async (task) => {
-  await axios.post("http://localhost:4000/tasks", task);
+  await axios.post(`${API_URL}/tasks`, task);
 };
 
 export const getTasksRequest = async () => {
-  const response = await axios.get("http://localhost:4000/tasks");
+  const response = await axios.get(`${API_URL}/tasks`);
   return response.data;
 };
 
 export const deleteTaskRequest = async (id) => {
-  await axios.delete(`http://localhost:4000/tasks/${id}`);
+  await axios.delete(`${API_URL}/tasks/${id}`);
 };
 
 export const updateTaskRequest = async (id, task) => {
-  return await axios.put(`http://localhost:4000/tasks/${id}`, task);
+  return await axios.put(`${API_URL}/tasks/${id}`, task);
 };
 
 export const getTaskRequest = async (id) => {
-  const response = await axios.get(`http://localhost:4000/tasks/${id}`);
+  const response = await axios.get(`${API_URL}/tasks/${id}`);
   return response.data;
 };
 
 export const toggleTaskDoneRequest = async (id, done) => {
-  await axios.put(`http://localhost:4000/tasks/${id}`, { done });
+  await axios.put(`${API_URL}/tasks/${id}`, { done });
 };
