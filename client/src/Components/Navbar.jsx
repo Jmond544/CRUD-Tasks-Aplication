1b68acedd50b231cf3e7aa0d0dce18635a5c71ff
Jmond544 <jmondalgotapia@gmail.com>
2023-12-25 20:27:41 -0500

    Style navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="bg-zinc-900 text-white flex justify-between p-5 items-center">
         <h1>React MySQL</h1>
-        <ul>
+        <ul className="flex gap-4 items-center">
             <li>
                 <Link to = "/">Home</Link>
             </li>
-            <li>
+            <li className="bg-zinc-950 p-2 rounded-xl">
                 <Link to = "/new">Create task</Link>
             </li>
         </ul>
