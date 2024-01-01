a484c6bf890ac651f935e63ffbae87561aab454d
Jmond544 <jmondalgotapia@gmail.com>
2024-01-01 03:43:56 -0500

    Adding catch to expired token

--- server/controllers/verifyToken.js
+++ server/controllers/verifyToken.js
@@ -6,7 +6,20 @@ export function verifyToken(req, res, next) {
   if (!token) {
     return res.status(403).json({ message: "No token provided" });
   }
-  const decoded = jwt.verify(token, SECRET_KEY);
+  const decoded = jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return "Expired";
+      } else {
+        console.error('Error al verificar el token:', err);
+      }
+    }else{
+      return decoded;
+    }
+  });
+  if(decoded === "Expired"){
+    return res.status(401).json({ message: "Expired" });
+  }
   req.userId = decoded.id;
   next();
 }
